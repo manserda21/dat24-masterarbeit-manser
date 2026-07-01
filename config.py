c531bdb5837602f6aa7677850743fcc46d5c6162
Daniel Manser <daniel.manser@edu.fh-joanneum.at>
2026-07-01 06:48:36 +0000

    Feat: added configs

--- config.py
+++ config.py
@@ -4,3 +4,15 @@ BASE_DATA = "/data/manser"
 RUNS_DIR = f"{BASE_DATA}/runs"
 DATASETS_DIR = f"{BASE_DATA}/datasets"
 VIDEOS_DIR = f"{BASE_DATA}/videos"
+
+# neu
+MODELS_DIR = f"{BASE_DATA}/models"
+VENVS_DIR = f"{BASE_DATA}/venvs"
+TRACKING_DIR = f"{BASE_DATA}/tracking_outputs"
+
+PNLCALIB_DIR = f"{BASE_DATA}/models/pnlcalib"
+PNLCALIB_WEIGHTS_KP = f"{PNLCALIB_DIR}/SV_kp"
+PNLCALIB_WEIGHTS_LINES = f"{PNLCALIB_DIR}/SV_lines"
+
+BASE_CODE = "/home/manser/projects/dat24-masterarbeit-manser"
+PNLCALIB_REPO = f"{BASE_CODE}/PnLCalib"
